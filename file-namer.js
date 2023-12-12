f303e4cb5689a72afb4a936dc962ff241bd42aed
BakerAO <dpshunter@gmail.com>
2023-12-11 21:49:40 -0600

    Update

--- file-namer.js
+++ file-namer.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 function renameMP4Files(directoryPath) {
   fs.readdir(directoryPath, (err, files) => {
